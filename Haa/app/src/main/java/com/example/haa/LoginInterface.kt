b021c321bbf72082b924057437413fd75a034d7e
janidu chamika <janchamika1@gmail.com>
2025-01-13 01:41:08 +0530

    update login registration janidu v1

--- Haa/app/src/main/java/com/example/haa/LoginInterface.kt
+++ Haa/app/src/main/java/com/example/haa/LoginInterface.kt
@@ -1,12 +1,31 @@
 package com.example.haa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class LoginInterface : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.login_interface)
+        val createAccountBtn=findViewById<TextView>(R.id.createAcountText)
+        createAccountBtn.setOnClickListener {
+            goToRegisterScreen()
+        }
+
+        val loginButton=findViewById<TextView>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            goToHomeScreen()
+        }
+    }
+    private fun goToRegisterScreen(){
+       val  myintent=Intent(this,Registration::class.java)
+        startActivity(myintent)
+    }
+    private fun goToHomeScreen(){
+        val  myintent=Intent(this,MainActivity::class.java)
+        startActivity(myintent)
     }
 }
--- Haa/app/src/main/java/com/example/haa/Registration.kt
+++ Haa/app/src/main/java/com/example/haa/Registration.kt
@@ -1,6 +1,29 @@
 package com.example.haa
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Registration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration)
+
+        val signupButton=findViewById<TextView>(R.id.signupButton)
+        signupButton.setOnClickListener {
+            goToLoginScreen()
+        }
+
+        val loginButton=findViewById<TextView>(R.id.loginText)
+        loginButton.setOnClickListener {
+            goToLoginScreen()
+        }
+    }
+    private fun goToLoginScreen(){
+        val  myintent= Intent(this,LoginInterface::class.java)
+        startActivity(myintent)
+    }
+
+
 }
