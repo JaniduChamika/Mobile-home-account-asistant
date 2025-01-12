3add34120066ee4b49d7b95c7c0d2f7e8324c05f
janidu chamika <janchamika1@gmail.com>
2025-01-13 00:03:08 +0530

    adding new fainace fragment janidu v1
    home page, adding new income,expence, date chooser.

--- Haa/app/src/main/java/com/example/haa/ViewyExpensesHistory.kt
+++ Haa/app/src/main/java/com/example/haa/ViewyExpensesHistory.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class home : Fragment() {
+class ViewyExpensesHistory : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_viewy_expenses_history, container, false)
     }
+
 
 }
