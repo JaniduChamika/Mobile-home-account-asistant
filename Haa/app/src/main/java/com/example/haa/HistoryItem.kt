98bf2baf92ceb30986ccb8b929a258298a9a88b8
janidu chamika <janchamika1@gmail.com>
2025-01-13 19:58:15 +0530

    malisious file remove

--- Haa/app/src/main/java/com/example/haa/HistoryItem.kt
+++
@@ -1,3 +0,0 @@
-package com.example.haa
-
-data class HistoryItem(val title: String, val price: String,val date:String,val note:String)
--- Haa/app/src/main/java/com/example/haa/HistoryItemIncome.kt
+++
@@ -1,3 +0,0 @@
-package com.example.haa
-
-data class HistoryItemIncome (val title: String, val price: String,val date:String)
